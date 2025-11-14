4d85ae824112c1d4c805c8373b8fba653ae402ab
wosix <wojtekkk00@o2.pl>
2025-11-14 09:04:35 +0100

    Mix colors

--- headers/ColorManager.h
+++ headers/ColorManager.h
@@ -21,3 +21,13 @@ Color Brighten(Color color, float factor)
         (unsigned char)std::min(255, (int)(color.b * factor)),
         100};
 }
+
+Color MixColors(Color a, Color b, float ratio = 0.5f)
+{
+    Color result;
+    result.r = a.r * (1 - ratio) + b.r * ratio;
+    result.g = a.g * (1 - ratio) + b.g * ratio;
+    result.b = a.b * (1 - ratio) + b.b * ratio;
+    result.a = a.a * (1 - ratio) + b.a * ratio;
+    return result;
+}
